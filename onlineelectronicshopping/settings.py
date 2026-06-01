6afb961b8111a4f951aabb8c6c2a65bb77cd3d3b
Benjamin J <benchamins2k5@gmail.com>
2026-06-01 22:01:05 +0530

    Production settings

--- onlineelectronicshopping/settings.py
+++ onlineelectronicshopping/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e_+laa7zdn)_fmsd8o%t1ecaug03^oy6(5-q!w!(6i%l10kv6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'onlineelectronicshopping/static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
